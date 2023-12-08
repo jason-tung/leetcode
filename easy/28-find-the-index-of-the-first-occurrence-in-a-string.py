33cc78f277076aeeee79c14401c66b2bb0fadc8b
jason-tung <jasontungnyc@gmail.com>
2023-12-07 23:49:32 -0500

    removed debugging comment

--- easy/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ easy/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,7 +1,7 @@
 #https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-      #haystack #kkk
+      #haystack
         for k in range(len(haystack)-len(needle)+1):
           flag = True
           for i in range(len(needle)):
